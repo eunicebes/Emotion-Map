0f57307b73402e6a07395c7f0b52ae052eefe370
eunicebes <eunicebes@gmail.com>
2017-03-25 19:28:22 +0800

    Write map function

---
+++ js/map.js
@@ -0,0 +1,44 @@
+function myMap() {
+    var inilat = 25.037;
+    var inilng = 121.56693799999994;
+    var option = {
+        enableHighAccuracy: true,
+        maximumAge: 600000,
+        timeout: 5000
+    }
+    if (navigator.geolocation) {
+        //navigator.geolocation.getCurrentPosition(showPosition, showError, option);
+        initMap(inilat, inilng);
+    } else {
+        //alert("Geolocation is not supported by this browser.");
+        initMap(inilat, inilng);
+    }
+}
+
+function showError(error) {
+    var inilat = 25.037;
+    var inilng = 121.56693799999994;
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            //alert("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            //alert("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            //alert("The request to get user location timed out.");
+            break;
+        case error.UNKNOWN_ERROR:
+            //alert("An unknown error occurred.");
+            break;
+    }
+    //showmap(inilat, inilng);
+}
+function initMap(inilat, inilng) {
+    // Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: inilat, lng: inilng},
+        scrollwheel: false,
+        zoom: 8
+    });
+}
