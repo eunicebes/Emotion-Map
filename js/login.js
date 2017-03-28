82e918a1cbb7898b9e233412cbd7d80b52edcbed
Chen YiHsin <yihsin@Eunice-MBP.local>
2017-03-28 22:09:58 +0800

    Write facebook function

--- js/login.js
+++ js/login.js
@@ -50,3 +50,32 @@ function fb_login(){
       }
   }, {scope: 'public_profile,email,user_friends,user_likes'});
 }
+
+// This is called with the results from from FB.getLoginStatus().
+function statusChangeCallback(response) {
+  console.log(response);
+  // The response object is returned with a status field that lets the
+  // app know the current login status of the person.
+  // Full docs on the response object can be found in the documentation
+  // for FB.getLoginStatus().
+  if (response.status === 'connected') {
+    // Logged into your app and Facebook.
+    console.log(response.authResponse.accessToken);
+     token = response.authResponse.accessToken;
+    FB.api('/me', {fields: 'name,gender,birthday,posts'}, function(response) {
+      //user的資料在這裡
+      console.log(response);
+      get_user(response, token)
+      // ajax and redirect to index.html
+    });
+  } else if (response.status === 'not_authorized') {
+    // The person is logged into Facebook, but not your app.
+    document.getElementById('status').innerHTML = 'Please log ' +
+      'into this app.';
+  } else {
+    // The person is not logged into Facebook, so we're not sure if
+    // they are logged into this app or not.
+    document.getElementById('status').innerHTML = 'Please log ' +
+      'into Facebook.';
+  }
+}
