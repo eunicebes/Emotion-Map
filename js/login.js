ae36c2c9fbde7f8ac7d51b210d955813456926f7
eunicebes <eunicebes@gmail.com>
2017-03-29 22:45:09 +0800

    Modify login function

--- js/login.js
+++ js/login.js
@@ -1,28 +1,26 @@
 window.fbAsyncInit = function() {
-FB.init({
-  appId      : '247334179013123',
-  cookie     : true,  // enable cookies to allow the server to access
-                      // the session
-  xfbml      : true,  // parse social plugins on this page
-  version    : 'v2.8' // use graph api version 2.8
-});
+  FB.init({
+    appId      : '390157798032692',
+    cookie     : true,  // enable cookies to allow the server to access
+                        // the session
+    xfbml      : true,  // parse social plugins on this page
+    version    : 'v2.8' // use graph api version 2.8
+  });
 
-// Now that we've initialized the JavaScript SDK, we call
-// FB.getLoginStatus().  This function gets the state of the
-// person visiting this page and can return one of three states to
-// the callback you provide.  They can be:
-//
-// 1. Logged into your app ('connected')
-// 2. Logged into Facebook, but not your app ('not_authorized')
-// 3. Not logged into Facebook and can't tell if they are logged into
-//    your app or not.
-//
-// These three cases are handled in the callback function.
-
-FB.getLoginStatus(function(response) {
-  statusChangeCallback(response);
-});
-
+  // Now that we've initialized the JavaScript SDK, we call
+  // FB.getLoginStatus().  This function gets the state of the
+  // person visiting this page and can return one of three states to
+  // the callback you provide.  They can be:
+  //
+  // 1. Logged into your app ('connected')
+  // 2. Logged into Facebook, but not your app ('not_authorized')
+  // 3. Not logged into Facebook and can't tell if they are logged into
+  //    your app or not.
+  //
+  // These three cases are handled in the callback function.
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
 };
 
 // Load the SDK asynchronously
@@ -49,6 +47,7 @@ function fb_login(){
         console.log('User cancelled login or did not fully authorize.');
       }
   }, {scope: 'public_profile,email,user_friends,user_likes'});
+
 }
 
 // This is called with the results from from FB.getLoginStatus().
@@ -65,17 +64,15 @@ function statusChangeCallback(response) {
     FB.api('/me', {fields: 'name,gender,birthday,posts'}, function(response) {
       //user的資料在這裡
       console.log(response);
-      get_user(response, token)
+      // get_user(response, token)
       // ajax and redirect to index.html
     });
   } else if (response.status === 'not_authorized') {
     // The person is logged into Facebook, but not your app.
-    document.getElementById('status').innerHTML = 'Please log ' +
-      'into this app.';
+      console.log('Please log into this app.');
   } else {
     // The person is not logged into Facebook, so we're not sure if
     // they are logged into this app or not.
-    document.getElementById('status').innerHTML = 'Please log ' +
-      'into Facebook.';
+      console.log('Please log into Facebook.');
   }
 }
